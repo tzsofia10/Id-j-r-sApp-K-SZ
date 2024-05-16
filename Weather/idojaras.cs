a38e253607847f0fb831cca0185716c16cf6a755
Zsófia Szofia <tzsofia07@gmail.com>
2024-05-16 17:28:38 +0200

    Add project files.

---
+++ Weather/idojaras.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather
+{
+    internal class idojaras
+    {
+        public string Varos {  get; set; }
+        public double Homerseklet { get; set;}
+        public int Paratartalom { get; set; }
+        public double Szelsebesseg { get; set; }
+
+
+        public idojaras(string sorok)
+        {
+            var sor= sorok.Split(';');
+            Varos = sor[0];
+            Homerseklet = Convert.ToDouble(sor[1]);
+            Paratartalom = Convert.ToInt32(sor[2]);
+            Szelsebesseg = Convert.ToDouble(sor[3]);
+                
+        }
+    }
+}
